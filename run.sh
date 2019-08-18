c16b39d9c1fda760ed29448a766ed82ab3aa9424
Jeremy Gousse <jeremy.gousse@gmail.com>
2019-08-18 09:57:35 -0400

    Starting on wanted days.

--- run.sh
+++ run.sh
@@ -4,9 +4,35 @@
 run_time_secs=$1
 musics_dir=$2
 start_time=$3
+active_days_input=$4
 
 start_time_hour=`echo $start_time | sed -e 's/:/\n/g' | sed -n 1p`
 start_time_mins=`echo $start_time | sed -e 's/:/\n/g' | sed -n 2p`
+
+active_days=()
+IFS=',' read -ra active_days <<< "$active_days_input"
+
+echo "Alarm set for $start_time_hour:$start_time_mins."
+echo "Will play music from: $musics_dir"
+echo "Will play for $run_time_secs secs."
+
+days=("Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday" "Sunday")
+for i in "${!days[@]}"; do
+	echo "Active on ${days[$i]}: ${active_days[$i]}"
+done
+
+function isActiveDay
+{
+	value=$1
+	active_day=false
+
+	for i in "${!days[@]}"; do
+		if [[ "${days[$i]}" = "${value}" ]]; then
+			active_day=true
+		  return
+		fi
+	done
+}
 
 function startAlarm
 {
@@ -25,19 +51,25 @@ function startAlarm
 	kill $pid
 }
 
-echo "Alarm set for $start_time_hour:$start_time_mins."
-echo "Will play music from: $musics_dir"
-echo "Will play for $run_time_secs secs."
-
 while true; do
+	current_day=`date +"%A"`
 	current_hour=`date +"%H"`
 	current_mins=`date +"%M"`
 
+	isActiveDay $current_day
+
+	if [ "$active_day" = "true" ]; then
+		sleep_time=`expr 23 - $current_hour + $start_time_hour`
+		sleep_time=`expr $sleep_time \* 3600`
+		echo "sleeping daily for $sleep_time"
+		sleep $sleep_time
+	fi
+
 	if [ "$current_hour" = "$start_time_hour" ] &&
-       [ "$current_mins" = "$start_time_mins" ]; then
+     [ "$current_mins" = "$start_time_mins" ]; then
 		startAlarm
 		sleep 61
-    fi
+  fi
 
 	sleep 59
 done
--- systemd/install.sh
+++ systemd/install.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
 if [ "$#" -ne 2 ]; then
-  echo "Usage: install.sh musics_dir run_time_secs start_time"
+  echo "Usage: install.sh musics_dir run_time_secs start_time active_days"
+  echo "- musics_dir: path to a directory full of music files/directories."
+  echo "- run_time_secs: Time (in seconds) to run music."
+  echo "- start_time: Time to start the alarm (exemple 07 for 7am or 20 for 8pm)."
+  echo "- active_days: Days alarm should trigger. List of 7 booleans starting from Monday. Example to activate weekly alarm: 1,1,1,1,1,0,0."
   exit 1
 fi
 
 musics_dir=$1
 run_time_secs=$2
 start_time=$3
+active_days=$4
 
 service_name=alarm_musics.service
 service_filepath=/lib/systemd/system/$service_name
@@ -19,6 +24,7 @@ service_content=`echo "${service_content/__PATH__/$script_path}"`
 service_content=`echo "${service_content/__MUSICS_DIR__/$musics_dir}"`
 service_content=`echo "${service_content/__RUN_TIME_SECS__/$run_time_secs}"`
 service_content=`echo "${service_content/__START_TIME__/$start_time}"`
+service_content=`echo "${service_content/__ACTIVE_DAYS__/$active_days}"`
 
 echo "$service_content" > $service_filepath
 chmod u+x $service_filepath
